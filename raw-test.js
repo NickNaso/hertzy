2e21ffe8afb3edc34a6e2ffc8ca786db50efa5bf
NickNaso <nicoladelgobbo@gmail.com>
2017-12-12 00:05:34 +0100

    Raw test to use in development phase

---
+++ raw-test.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Hertzy = require('./')
+
+const usrFreq = Hertzy.tune('user')
+usrFreq.on('user:add', function (data) {
+    console.log('NEW USER ADDED WITH FOLLOWING DATA:')
+    console.log(data)
+})
+
+const usrFreq1 = Hertzy.tune('user')
+usrFreq1.on('user:add', function (data) {
+    console.log('NEW USER ADDED WITH FOLLOWING DATA:')
+    console.log(data)
+})
+
+usrFreq.emit('user:add', {
+    username: 'NickNaso',
+    password: '********',
+    email: 'nicoladelgobbo@gmail.com'
+})
+
+
+
+
